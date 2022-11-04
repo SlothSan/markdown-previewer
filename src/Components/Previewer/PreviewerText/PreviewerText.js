95be8e4d6130f3f08d3c6a769bb6bac4826f58cf
Mike Oram <Mikeoram01@gmail.com>
2022-11-04 17:41:21 +0000

    added lowlight and highlight packages

--- src/Components/Previewer/PreviewerText/PreviewerText.js
+++ src/Components/Previewer/PreviewerText/PreviewerText.js
@@ -1,8 +1,19 @@
 import './PreviewerText.css'
 import Markdown from "marked-react";
+import Lowlight from 'react-lowlight';
+import javascript from 'highlight.js/lib/languages/javascript';
 
 
 const PreviewerText = (props) => {
+
+    Lowlight.registerLanguage('js', javascript);
+
+    const renderer = {
+        code(snippet, lang) {
+            return <Lowlight key={this.elementId} language={lang} value={snippet} />;
+        },
+    };
+
 
     return (
         <div id={"preview"} className={"preview-text"}>
